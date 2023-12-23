41fca7e49bb0587cde7fe6b61ca41bc7e4734b54
Patrick Haun <patrick@bomgar.email>
2023-12-23 15:01:22 +0100

    wip: save new user

--- services/authservice/authservice.go
+++ services/authservice/authservice.go
@@ -1,7 +1,23 @@
 package authservice
 
-import "log/slog"
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/bomgar/basicwebapp/web/dto"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type AuthService struct {
 	Logger *slog.Logger
+}
+
+func (s *AuthService) Register(registerRequest dto.RegisterRequest) error {
+
+	_, err := bcrypt.GenerateFromPassword([]byte(registerRequest.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("failed to hash password: %w", err)
+	}
+
+	return nil
 }
