ef7755d6fe186a1245fc532ee7ea27ff406d773d
Patrick Haun <patrick@bomgar.email>
2023-12-23 22:28:51 +0100

    refactor: minor cleanup

--- services/authservice/authservice.go
+++ services/authservice/authservice.go
@@ -23,7 +23,7 @@ func (s *AuthService) Register(ctx context.Context, registerRequest dto.Register
 		return fmt.Errorf("failed to hash password: %w", err)
 	}
 
-	err = s.DB.AcquireFunc(ctx, func(conn *pgxpool.Conn) error {
+	return s.DB.AcquireFunc(ctx, func(conn *pgxpool.Conn) error {
 		queries := q.New(conn)
 		_, err = queries.InsertUser(ctx, q.InsertUserParams{
 			Email:          registerRequest.Email,
@@ -34,31 +34,23 @@ func (s *AuthService) Register(ctx context.Context, registerRequest dto.Register
 		}
 		return nil
 	})
-
-	if err != nil {
-		return fmt.Errorf("Registration failed: %w", err)
-	}
-
-	return nil
 }
 
 func (s *AuthService) Login(ctx context.Context, loginRequest dto.LoginRequest) (int32, error) {
-	var userId int32
-	var passwordHash string
-	err := s.DB.AcquireFunc(ctx, func(conn *pgxpool.Conn) error {
-		queries := q.New(conn)
-		row, err := queries.SelectPasswordHashByUserEmail(ctx, loginRequest.Email)
-		if err != nil {
-			return fmt.Errorf("Could not retrieve password hash: %w", err)
-		}
-		passwordHash = row.HashedPassword
-		userId = row.ID
-		return nil
-	})
-
+	conn, err := s.DB.Acquire(ctx)
 	if err != nil {
-		return -1, fmt.Errorf("Could not retrieve passwort hash: %w", err)
+		return -1, err
 	}
+	defer conn.Release()
+	queries := q.New(conn)
+
+	row, err := queries.SelectPasswordHashByUserEmail(ctx, loginRequest.Email)
+	if err != nil {
+		return -1, fmt.Errorf("Could not retrieve password hash: %w", err)
+	}
+
+	passwordHash := row.HashedPassword
+	userId := row.ID
 
 	err = bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(loginRequest.Password))
 	if err != nil {
